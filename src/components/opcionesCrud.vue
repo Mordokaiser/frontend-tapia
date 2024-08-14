194e3b63e6df4d7396de6dd2c5f4b420e99037e7
fpinilla198 <148462195+fpinilla198@users.noreply.github.com>
2024-08-14 10:54:57 -0400

    correxion de diseño y se añade Axios y metodo post

--- src/components/opcionesCrud.vue
+++ src/components/opcionesCrud.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="">
-        <h1>Xiaomi phones</h1>
-        <p>Sistema gestor de teléfonos en stock</p>
+    <div class=" bg-slate-100 rounded-md px-4 py-4 mb-8 shadow-sm font-mono">
+        
+
+        <p class="mb-8">Sistema gestor de teléfonos en stock, este sistema esta pensado para que pueda mantener
+            actualizado la informacion de stock que tiene en sus tiendas.</p>
         <!-- opciones del crud -->
-        <div class="grid grid-cols-5">
-            <div class="grid-row">
-                <label for="model">modelo</label>
-                <select id="model" name="model" v-model="modelRef">
+        <div class="grid md:grid-cols-5">
+            <div>
+                <label for="model">Modelo: </label>
+                <select id="model" name="model" v-model="modelRef" class="rounded-md border whitespace-nowrap max-w-20 lg:max-20 xl2:max-w-40">
                     <option value="Redmi_Note_10">Redmi Note 10</option>
-                    <option value="Redmi_Note10_Pro">Redmi Note 10 Pro</option>
+                    <option value="Redmi_Note_10_Pro">Redmi Note 10 Pro</option>
                     <option value="Redmi_Note_10_Pro_Max">Redmi Note 10 Pro Max</option>
                     <option value="Redmi_Note_10_5G">Redmi Note 10 5G</option>
                 </select>
             </div>
             <div>
-                <label>color</label>
-                <select id="color" name="color" v-model="colorRef">
+                <label>Color: </label>
+                <select id="color" name="color" v-model="colorRef" class="rounded-md borde max-w-20 lg:max-20 xl2:max-w-40 whitespace-nowrap ">
                     <option value="gray">Onyx Gray</option>
                     <option value="white">Pebble White</option>
                     <option value="green">Lake Green</option>
@@ -23,16 +25,16 @@
                 </select>
             </div>
             <div>
-                <label for="price">precio</label>
-                <input type="number" id="price" name="price" min="0" max="9999" v-model.number="priceRef">
+                <label for="price">Precio: </label>
+                <input type="number" id="price" name="price" min="0" max="9999" v-model.number="priceRef" class="rounded-md borde">
             </div>
             <div>
-                <label for="stock">stock</label>
-                <input type="number" id="stock" name="stock" min="0" max="9999" v-model.number="stockRef">
+                <label for="stock">Stock: </label>
+                <input type="number" id="stock" name="stock" min="0" max="9999" v-model.number="stockRef" class="rounded-md borde">
             </div>
-            <div>
-                <label>tienda</label>
-                <select id="store" name="store" v-model="storeRef">
+            <div >
+                <label>Tienda: </label>
+                <select id="store" name="store" v-model="storeRef" class="rounded-md borde max-w-20 lg:max-20 xl2:max-w-40 whitespace-nowrap ">
                     <option value="abcdin">abcdin</option>
                     <option value="paris">paris</option>
                     <option value="falabella">falabella</option>
@@ -42,7 +44,7 @@
         </div>
         <div class="flex w-full justify-center mt-4 mb-2">
             <button
-                class="bg-sky-600 rounded-md px-4 py-1 flex items-center uppercase hover:ring-4 hover:ring-green-400"
+                class="bg-slate-100 border border-orange-600 rounded-md px-4 py-1 flex items-center uppercase hover:shadow-sm hover:shadow-orange-600"
                 @click="addPhone">agregar</button>
         </div>
     </div>
@@ -58,45 +60,27 @@ const priceRef = ref(990);
 const stockRef = ref(10);
 const storeRef = ref('');
 
+const base_url = 'http://app.backend-tapia/api';
 const addPhone = async () => {
-    console.log('Agregando teléfono...');
+    //enviar por axios
+    const phoneData = new FormData();
+    phoneData.append('model', modelRef.value);
+    phoneData.append('color', colorRef.value);
+    phoneData.append('price', priceRef.value);
+    phoneData.append('stock', stockRef.value);
+    phoneData.append('store', storeRef.value);
 
-    // Datos del teléfono que se enviarán
-    const phoneData = {
-        model: modelRef.value,
-        color: colorRef.value,
-        price: priceRef.value,
-        stock: stockRef.value,
-        store: storeRef.value
-    };
-
-    // Convertir los datos a una cadena de consulta
-    const queryString = new URLSearchParams(phoneData).toString();
-
-    const base_url = 'http://app.backend-tapia';
     try {
-        // Construir la URL completa con los parámetros de consulta
-        const url = `${base_url}/add-phones?${queryString}`;
-
-        // Envío de los datos al servidor usando fetch
-        const response = await fetch(url, {
-            method: 'GET', // Método GET
+        const response = await axios.post(`${base_url}/add-phones`, phoneData, {
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'multipart/form-data'
             }
         });
-
-        // Verificar si la respuesta es exitosa
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        // Parsear la respuesta JSON
-        const data = await response.json();
-        console.log('Teléfono agregado exitosamente:', data);
+        console.log('Teléfono agregado exitosamente:', response.data);
     } catch (error) {
         console.error('Error al agregar el teléfono:', error);
     }
-};
 
+}
 </script>
+<style scoped></style>
--- src/components/principalTemplate.vue
+++ src/components/principalTemplate.vue
@@ -1,7 +1,12 @@
 <template>
 
-    <div class="flex h-screen justify-center bg-cyan-950 ">
-        <div class="bg-slate-400 my-8 w-3/4 ">
+    <div class="flex  justify-center">
+
+        <div class="bg-slate-200 my-8 w-3/4 rounded-md px-8 pt-20 shadow-md shadow-orange-500">
+            <div class="flex justify-center">
+                <img src="img/xiaomi-logo.webp" alt="logo" class="size-10">
+                <h1 class="font-bold text-[24px] mb-8">Xiaomi phones</h1>
+            </div>
             <slot></slot>
         </div>
 
